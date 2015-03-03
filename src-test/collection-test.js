346d92dff64fce53cb543285056ec40043d6b9d5
Richard <burkhardt@konsultaner.de>
2015-03-03 12:57:00 +0100

    test for collection array inheritance

--- src-test/collection-test.js
+++ src-test/collection-test.js
@@ -7,5 +7,11 @@ jsonOdm.addSource("test",{"testCollection":[
 
 CollectionTest.prototype.testArrayInheritance = function () {
     var collection = new jsonOdm.Collection("testCollection");
-    assertEquals("LengthTest",2,collection.length);
+    assertEquals("LengthTest1",2,collection.length);
+    collection[0] = {name:"Mustermann-1"};
+    collection[1] = {name:"Mustermann-2"};
+    collection[2] = {name:"Mustermann-3"};
+    collection.push({name:"Mustermann-4"});
+    assertEquals("LengthTest2",4,collection.length);
+    assertEquals("ContentTest","Mustermann-2",collection[1].name);
 };
