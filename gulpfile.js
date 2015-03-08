8f518b09bff55f0b597a08588c91294be91900e8
Richard <burkhardt@konsultaner.de>
2015-03-08 14:01:13 +0100

    doc enhancements

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,26 @@
 "use strict";
 
 var gulp = require('gulp'),
+    jsdoc = require("gulp-jsdoc"),
     concat = require('gulp-concat'),
+    filesize = require('gulp-filesize'),
     uglify = require('gulp-uglify');
 
 gulp.task('default', function() {
     return gulp.src(['./src/odm.js', './src/util.js', './src/collection.js', './src/query.js'])
+        .pipe(filesize())
+        .pipe(jsdoc.parser({
+            name:"jsonOdm",
+            description:"A light weight object document mapper for JavaScript objects.",
+            version:"0.0.1"
+        }, "jsonOdm"))
+        .pipe(jsdoc.generator('./doc',
+            {
+                path: './node_modules/jsdoc3-bootstrap'
+            })
+        )
         .pipe(concat('json.odm.js'))
         .pipe(uglify())
+        .pipe(filesize())
         .pipe(gulp.dest('./bin/'));
 });
--- src/odm.js
+++ src/odm.js
@@ -1,15 +1,66 @@
 "use strict";
 
+/**
+ * The main class holding all sub classes and the data source
+ * @version 0.0.1
+ * @author Richard Burkhardt - Konsultaner
+ *
+ * @constructor
+ * @example
+ * var odm = new jsonOdm();
+ * odm.addSource('people',{
+ *    "Person" : [
+ *        {"id":1,"name":"Richi",jobId:1},
+ *        {"id":2,"name":"Dave",jobId:2},
+ *        {"id":3,"name":"Tom",jobId:3},
+ *        {"id":4,"name":"Lisa",jobId:4},
+ *        {"id":5,"name":"Hanni",jobId:3},
+ *        {"id":6,"name":"Selma",jobId:3},
+ *        {"id":7,"name":"Ralf",jobId:1}
+ *    ],
+ *    "Jobs" : [
+ *        {"id":1,"name":"plumber"},
+ *        {"id":2,"name":"programmer"},
+ *        {"id":3,"name":"chef"},
+ *        {"id":4,"name":"hairdresser"}
+ *    ]
+ * });
+ *
+ * var people = new jsonOdm.Collection('Person');
+ * people.$hasOne("jobId","id","Jobs","job");
+ *
+ * var q = people.query();
+ * // get all hairdresser
+ * var hairdresser = q.$or(
+ *    q.$branch("jobId").$eq(4)
+ * ).$all();
+ *
+ * // get all but hairdressers
+ * var hairdresser = q.$or(
+ *    q.$branch("jobId").$notEq(4)
+ * ).$all();
+ *
+ */
 function JsonOdm () {
     var self = this;
     this.sources = {};
     this.selectedSource = {};
+    /**
+     * Add a data source to the odm that is selectable via the selectSource method
+     * @param {*} sourceId There is an override check
+     * @param {object} source An object with collection arrays
+     * @param {boolean} selectSource select the source
+     */
     this.addSource = function (sourceId, source, selectSource) {
         if (typeof source == "object") {
             if (typeof self.sources[sourceId] == "undefined") self.sources[sourceId] = source;
             if(selectSource) self.selectedSource = source;
         }
     };
+    /**
+     * Select a source by its id this will effect all collections
+     * @param {*} sourceId
+     */
     this.selectSource = function (sourceId) {
         if(typeof self.sources[sourceId] != "undefined") self.selectedSource = self.sources[sourceId];
     };
